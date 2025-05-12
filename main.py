4cc15542fa330d245e3c3d65bcd5d55f2320f054
Lowe Berg <loweberg04@gmail.com>
2025-05-12 13:58:38 +0200

    Fixed initialization algorithm

--- main.py
+++ main.py
@@ -1,5 +1,43 @@
 # Hello, this is the main file
 
+import Sparse
+import numpy as np
+
 print("Grattis på födelsedagen Freja")
 for i in range(5):
-  print("Ja må hon leva")
+    print("Ja må hon leva")
+
+A = np.array([[0, 0, 0, 0],
+              [5, 8, 0, 0],
+              [0, 0, 3, 0],
+              [0, 6, 0, 0]])
+
+B = np.array([[10, 20, 0, 0, 0, 0],
+              [0, 30, 0, 4, 0, 0],
+              [0, 0, 50, 60, 70, 0],
+              [0, 0, 0, 0, 0, 80]])
+
+C = np.array([[0, 1, 1, 0, 0, 0, 0, 0],
+              [0, 0, 0, 8, 0, 0, 0, 0],
+              [19, 0, 0, 0, 0, 0, 5, 0],
+              [0, 0, 10, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 0, 1, 0],
+              [0, 2, 0, 0, 0, 17, 0, 0],
+              [0, 0, 1, 0, 0, 0, 0, 0],
+              [0, 0, 0, 0, 0, 1, 0, 0]
+              ])
+
+D = np.array([[0, 0, 0, 0, 1],
+              [5, 8, 0, 0, 0],
+              [0, 0, 3, 0, 0],
+              [0, 6, 0, 0, 1]])
+
+SA = Sparse.SparseMatrix(A)
+SB = Sparse.SparseMatrix(B)
+SC = Sparse.SparseMatrix(C)
+SD = Sparse.SparseMatrix(D)
+
+print(SA)
+print(SB)
+print(SC)
+print(SD)
